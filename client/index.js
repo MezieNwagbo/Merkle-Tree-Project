5fe7b06b40faaa475d4e00d26ad65a6f35eb3099
Mezie Nwagbo <chimezienwagbotech@gmail.com>
2023-09-02 16:20:02 +0100

    final push

--- client/index.js
+++ client/index.js
@@ -1,14 +1,27 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const verifyProof = require("../utils/verifyProof");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const { bytesToHex, toHex } = require("ethereum-cryptography/utils");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
+
+// get the root
+
+// find the proof that name is in the list
+const name = "Mr. Janice Ryan";
+const index = niceList.findIndex((n) => n === name);
+const proof = merkleTree.getProof(index);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
   console.log({ gift });
